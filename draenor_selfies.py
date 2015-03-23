46303d81039d4516ab7cc9a6d41d122579ea2623
tehspaceg <bitbox@geran.me>
2015-03-23 01:11:53 -0700

    Adding the license
    Licensed under GPL v3 now

--- draenor_selfies.py
+++ draenor_selfies.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3.4
 # -*- coding: utf-8 -*-
 
+### LICENSE ###
+# WoW Selfie Bot
+# Used to retweet selfies taken in World of Warcraft. The bot in production can be found here: https://twitter.com/WoWSelfieBot
+# Copyright (C) 2015  - Geran Smith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# If you have any issues, please post on the GitHub page for this project: <https://github.com/tehspaceg/DraenorSelfies>
+
 # imports the tweepy stuff, datetime for rate limit, and sys for reading a file
 import tweepy, sys, json
 from datetime import datetime
@@ -8,8 +28,8 @@ from datetime import datetime
 #enter the corresponding information from your Twitter application:
 consumer_key = '123456' #keep the quotes, replace this with your consumer key
 consumer_secret = '123456' #keep the quotes, replace this with your consumer secret key
-access_token = '123456' #keep the quotes, replace this with your access token
-access_token_secret = '123456' #keep the quotes, replace this with your access token secret
+access_token = '123456'#keep the quotes, replace this with your access token
+access_token_secret = '123456'#keep the quotes, replace this with your access token secret
 
 # We assume the text files exist and are in the same location as this script
 # Reads the "blocked_users" file and puts the data into the "blocked_users" variable, then closes the file (like a refrigerator)
